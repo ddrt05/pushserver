fac07b208d3aa1a614c1eaf05522e77ac47ae539
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-03 10:44:43 +0200

    Update webpush.js

--- src/webpush.js
+++ src/webpush.js
@@ -1,8 +1,8 @@
 const webpush = require("web-push");
 webpush.setVapidDetails(
   "mailto:druizt@externas.jccm.es",
- "BHz5Gre463ldKXVJ5-V3oB5c12zYNh7XA57oYVHzexTet29yg5XCweuVYKXNSKnTJtgKvVFgnG3lmrgodUbfjuA",
-  "270jLc8NEF_S7IcacvRoxExSpV6s2Jce_nECy8NrsMA"
+ "",
+  ""
 );
 
 module.exports = webpush;
