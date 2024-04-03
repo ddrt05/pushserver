58ca2764ec0a34c3cd2109c9c9c75cf62fcb6c98
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-03 10:46:39 +0200

    Update webpush.js

--- src/webpush.js
+++ src/webpush.js
@@ -1,8 +1,8 @@
 const webpush = require("web-push");
 webpush.setVapidDetails(
   "mailto:druizt@externas.jccm.es",
- "",
-  ""
+ "mailto:druizt@externas.jccm.es",
+  "mailto:druizt@externas.jccm.es"
 );
 
 module.exports = webpush;
