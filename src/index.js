f43817b26e93aef21b3258ac2f921ee51e31f8c2
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-04 10:35:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,6 @@ app.use(require('./routes/index'));
 
 // Contenido estático (Static content)
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', express.Router());
 
 app.listen(3000, function () {
     console.log('Server listening on port 3000!');
