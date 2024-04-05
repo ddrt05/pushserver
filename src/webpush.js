87cee884cd97e4bd8262669fcd4d0a81dcbb27b5
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-05 14:10:14 +0200

    Update webpush.js

--- src/webpush.js
+++ src/webpush.js
@@ -10,7 +10,7 @@ console.log ("publica " +publica);
 console.log ("privada " +privada);
  webpush.setVapidDetails(
    "mailto:druizt@externas.jccm.es",
-   publica,
+   "BHz5Gre463ldKXVJ5-V3oB5c12zYNh7XA57oYVHzexTet29yg5XCweuVYKXNSKnTJtgKvVFgnG3lmrgodUbfjuA",
    privada
  );
 
