88b422ce05e6c049b7a8880365834f71b2e0ceb6
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-05 14:11:28 +0200

    Update webpush.js

--- src/webpush.js
+++ src/webpush.js
@@ -11,7 +11,7 @@ console.log ("privada " +privada);
  webpush.setVapidDetails(
    "mailto:druizt@externas.jccm.es",
    "BHz5Gre463ldKXVJ5-V3oB5c12zYNh7XA57oYVHzexTet29yg5XCweuVYKXNSKnTJtgKvVFgnG3lmrgodUbfjuA",
-   privada
+   "270jLc8NEF_S7IcacvRoxExSpV6s2Jce_nECy8NrsMA"
  );
 
 function ByteArrayPadLeft(src, size)
