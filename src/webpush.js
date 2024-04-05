22f5807c85767bd41f7802928ce01fee732ecd27
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-05 13:49:56 +0200

    Update webpush.js

--- src/webpush.js
+++ src/webpush.js
@@ -1,11 +1,11 @@
  webpush = require("web-push");
- const base64url = require('base64-url');
+ const base64url = require('base64url');
 
 console.log (process.env.PRIVATE_VAPID_KEY);
 console.log (process.env.PUBLIC_VAPID_KEY);
 console.log ("nueva version 2");
-const publica = base64url.encode(process.env.PUBLIC_VAPID_KEY);
-const privada = base64url.encode(process.env.PRIVATE_VAPID_KEY);
+const publica = base64url.toBase64(process.env.PUBLIC_VAPID_KEY);
+const privada = base64url.toBase64(process.env.PRIVATE_VAPID_KEY);
 console.log ("publica " +publica);
 console.log ("privada " +privada);
  webpush.setVapidDetails(
