e9d9133338d0c0b7960db9664ef51b586a4f9922
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-05 13:25:19 +0200

    Update webpush.js

--- src/webpush.js
+++ src/webpush.js
@@ -6,8 +6,8 @@ console.log (process.env.PUBLIC_VAPID_KEY);
 console.log ("nueva version 2");
 //const publica = base64url.encode(process.env.PUBLIC_VAPID_KEY);
 //const privada = base64url.encode(process.env.PRIVATE_VAPID_KEY);
-console.log ("publica " +publica);
-console.log ("privada " +privada);
+//console.log ("publica " +publica);
+//console.log ("privada " +privada);
 // webpush.setVapidDetails(
  //  "mailto:druizt@externas.jccm.es",
  //  publica,
