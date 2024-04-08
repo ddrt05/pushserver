d2d5dbd5823a254aaddda0b3980854c91e88d5ec
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-08 10:24:06 +0200

    Update webpush.js

--- src/webpush.js
+++ src/webpush.js
@@ -7,16 +7,8 @@ console.log ("nueva version Puerto: " +process.env.PORT);
 
  webpush.setVapidDetails(
    "mailto:druizt@externas.jccm.es",
-    process.env.PRIVATE_VAPID_KEY,
+    ""+process.env.PRIVATE_VAPID_KEY+"",
     process.env.PRIVATE_VAPID_KEY
  );
-
-function ByteArrayPadLeft(src, size)
- {
-  var dst = new byte[size];
-  var startAt = dst.Length - src.Length;
-  Array.Copy(src, 0, dst, startAt, src.Length);
-  return dst;
-}
 
 module.exports = webpush;
