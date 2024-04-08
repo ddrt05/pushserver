4383bb3c810c4127fc6fb42fc5361aa456cadb10
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-08 10:25:48 +0200

    Update webpush.js

--- src/webpush.js
+++ src/webpush.js
@@ -1,5 +1,4 @@
 webpush = require("web-push");
-require('dotenv').config();
 
 console.log (process.env.PRIVATE_VAPID_KEY);
 console.log (process.env.PUBLIC_VAPID_KEY);
@@ -7,8 +6,8 @@ console.log ("nueva version Puerto: " +process.env.PORT);
 
  webpush.setVapidDetails(
    "mailto:druizt@externas.jccm.es",
-    ""+process.env.PRIVATE_VAPID_KEY+"",
-    process.env.PRIVATE_VAPID_KEY
+    "${process.env.PRIVATE_VAPID_KEY}",
+    ""+process.env.PRIVATE_VAPID_KEY+""
  );
 
 module.exports = webpush;
