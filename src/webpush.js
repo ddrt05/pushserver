5737d3792233619f12dd5e930929656774fcf37f
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-08 10:06:46 +0200

    Update webpush.js

--- src/webpush.js
+++ src/webpush.js
@@ -4,8 +4,7 @@
 console.log (process.env.PRIVATE_VAPID_KEY);
 console.log (process.env.PUBLIC_VAPID_KEY);
 console.log ("nueva version 2");
-console.log ("publica " +publica);
-console.log ("privada " +privada);
+
  webpush.setVapidDetails(
    "mailto:druizt@externas.jccm.es",
     process.env.PRIVATE_VAPID_KEY,
