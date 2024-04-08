a3464bc6bd55997906d2b551987ec8177b5f5e39
ddrt05 <64745643+ddrt05@users.noreply.github.com>
2024-04-08 10:28:30 +0200

    Update webpush.js

--- src/webpush.js
+++ src/webpush.js
@@ -7,7 +7,7 @@ console.log ("nueva version Puerto: " +process.env.PORT);
  webpush.setVapidDetails(
    "mailto:druizt@externas.jccm.es",
     process.env.PUBLIC_VAPID_KEY,
-    ""+process.env.PRIVATE_VAPID_KEY+""
+    process.env.PRIVATE_VAPID_KEY
  );
 
 module.exports = webpush;
